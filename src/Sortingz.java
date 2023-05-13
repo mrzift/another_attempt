a2896d05d01894d6c7856d478c351ff80ede4b5b
Modassir <padhnakhelna@gmail.com>
2023-05-13 17:00:20 +0300

    second commit

--- src/Sortingz.java
+++ src/Sortingz.java
@@ -3,22 +3,22 @@ public class Sortingz {
 
         //---Bubble sort---// Ascending Order
 
-//        int arr[] = {2, 9, 7, 5, 4};
-//        for (int i = 0; i < arr.length - 1; i++) {
-//            for (int j = 0; j < arr.length - i - 1; j++) {
-//                // Condition Check
-//                if (arr[j] > arr[j + 1]) {
-//                    // swap
-//                    int temp = arr[j];
-//                    arr[j] = arr[j + 1];
-//                    arr[j + 1] = temp;
-//                }
-//            }
-//        }
-//        // Printing the array
-//        for(int i=0; i<arr.length; i++) {
-//            System.out.print(arr[i]+" ");
-//        }
+        int arr[] = {2, 9, 7, 5, 4};
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                // Condition Check
+                if (arr[j] > arr[j + 1]) {
+                    // swap
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        // Printing the array
+        for(int i=0; i<arr.length; i++) {
+            System.out.print(arr[i]+" ");
+        }
 
         //---Bubble sort---// Descending Order
 
@@ -57,20 +57,19 @@ public class Sortingz {
 //            System.out.print(arr[i] + " ");
 //        }
 
-        int arr[] = {2, 5, 3, 9, 8};
-
-        for (int i = 1; i < arr.length; i++) {
-            int current = arr[i];
-            int j = i - 1;
-            while (j >= 0 && arr[j] > current) {
-                //Keep swapping
-                arr[j + 1] = arr[j];
-                j--;
-            }
-            arr[j + 1] = current;
-        }
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
+//        int arr[] = {2, 5, 3, 9, 8};
+//
+//        for (int i = 1; i < arr.length; i++) {
+//            int current = arr[i];
+//            int j = i - 1;
+//            while (j >= 0 && arr[j] > current) {
+//                //Keep swapping
+//                arr[j + 1] = arr[j];
+//                j--;
+//            }
+//            arr[j + 1] = current;
+//        }
+//        for (int i = 0; i < arr.length; i++) {
+//            System.out.print(arr[i] + " ");
         }
     }
-}
